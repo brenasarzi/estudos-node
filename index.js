8f2a87c1a7ab14bf3532af47f0145de4e754d56f
Brena Sarzi <sarzibrena@gmail.com>
2022-07-25 12:22:53 -0300

    extrair links do texto e erros de extração

---
+++ index.js
@@ -0,0 +1,30 @@
+const chalk = require('chalk')
+const fs = require('fs')
+
+function extraiLinks(texto){
+    const regex = /\[([^\]]*)\]\((https?:\/\/[^$#\s].[^\s]*)\)/gm
+    const arrayResultados = []
+    let temp
+    while((temp = regex.exec(texto)) !== null){
+      arrayResultados.push({ [temp[1]]: temp[2] }) 
+    }
+    return arrayResultados.length === 0 ? 'Não há links' : arrayResultados
+}
+
+function trataErro(erro){
+    // lançar erro
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no caminho'))
+}
+
+async function pegaArquivo(caminhoDoArquivo){
+    const enconding = "utf-8"
+    try{    
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        return extraiLinks(texto)
+    }
+    catch(erro){
+        trataErro(erro)
+    }
+}
+
+module.exports = pegaArquivo
